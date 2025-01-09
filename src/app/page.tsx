995a2e46d0385ab52984bbdc2b7eeb60ff2be67e
Rahul S Kumar <rahul.kumar@qburst.com>
2025-01-09 14:15:57 +0530

    Update css  and add transaction card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import SemiCircleProgressBar from "@/components/semiCircleProgressBar";
+import TransactionCard from "@/components/transactionCard";
+import Link from "next/link";
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <section>
+      <div className="mt-2">
+        <div className="rounded">
+          <SemiCircleProgressBar />
+        </div>
+        <div className="grid grid-cols-2 m-2 p-3 text-xs font-normal">
+          <div className="col-span-1">Recent Transactions</div>
+          <div className="text-right underline decoration-white/25">
+            <Link href={"/"}>View All</Link>
+          </div>
+        </div>
+        <div>
+          <TransactionCard success={false} />
+          <TransactionCard success={true} />
+        </div>
+      </div>
+    </section>
+  );
 }
