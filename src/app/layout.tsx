a21a17d478e9bd3a2aaa9528ff1b5ad6f2c2c358
Rahul S Kumar <84220247+rahulsk666@users.noreply.github.com>
2025-01-10 12:08:56 +0530

    Dashboard - Navbar, Radial Graph and Transaction-Card (#2)
    * initial setup
    
    * Add radial bar component to dashboard
    
    * Update linear gradient color stops fro semi circle progress bar
    
    * remove comments
    
    * update to export component and removed dashboard folder
    
    * Update css  and add transaction card
    
    * Add Appbar with initial design with only icons and changed navbar to header.
    
    ---------
    
    Co-authored-by: Rahul S Kumar <rahul.kumar@qburst.com>

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import AppBar from "@/components/appbar";
+import Header from "@/components/header";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "600"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Spendlux",
+  description: "Finance manager",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} bg-background text-white`}>
+        <div className="md:hidden">
+          <Header />
+          <main>{children}</main>
+          <AppBar />
+        </div>
+        <div className="hidden md:block text-center">
+          Only Supported in mobile devices
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,84 @@
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["var(--font-poppins)"],
+        montserrat: ["var(--font-montserrat)"],
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        appbar: {
+          primary: "var(--navigation-primary)",
+          secondary: "var(--navigation-secondary)",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+          gradient: {
+            "1": "hsla(var(--radial-graph-card-background-1))",
+            "2": "hsla(var(--radial-graph-card-background-2))",
+            "3": "var(--transaction-card-background-1)",
+            "4": "var(--transaction-card-background-2)",
+            "5": "var(--transaction-card-background-3)",
+            "6": "var(--transaction-card-symbol-background-1)",
+            "7": "var(--transaction-card-symbol-background-2)",
+          },
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+        font: {
+          "transaction-red": "var(--text-transaction-red)",
+          "transaction-green": "var(--text-transaction-green)",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate],
+} satisfies Config;
