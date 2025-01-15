435b8945e252d57d5444397fe779341d6bc1ea12
Rahul S Kumar <rahul.kumar@qburst.com>
2025-01-15 17:35:36 +0530

    Update app bar code

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -26,19 +26,16 @@ const IconButton: React.FC<IconButtonProps> = ({
   ...props
 }) => {
   const ref = useRef<HTMLSpanElement>(null);
+  const isClient = typeof window !== "undefined";
   return (
     <button
-      onMouseEnter={() => handleNav(iconName)}
-      onMouseLeave={() => handleNav(iconName)}
+      onClick={() => handleNav(iconName)}
       className={`
-        flex m-1 p-2 items-center align-middle rounded-full
-        text-white 
-        ${
-          activeNav == iconName
+        flex m-1 p-2 items-center align-middle rounded-full text-white ${
+          activeNav === iconName
             ? "bg-appbar-blue"
             : color || "bg-appbar-secondary"
-        }
-        ${className}
+        } ${className}
       `}
       {...props}
     >
@@ -46,10 +43,12 @@ const IconButton: React.FC<IconButtonProps> = ({
       <div
         style={{
           width:
-            activeNav == iconName ? (ref.current?.offsetWidth || 0) + 30 : 0,
+            isClient && activeNav == iconName
+              ? (ref.current?.offsetWidth || 0) + 30
+              : 0,
           height: "30px",
         }}
-        className={`overflow-x-hidden transition-all duration-200 ease-linear bg-inherit ${
+        className={`overflow-x-hidden transition-all duration-200 delay-100 ease-linear bg-inherit ${
           activeNav == iconName
             ? "bg-appbar-blue"
             : color || "bg-appbar-secondary"
