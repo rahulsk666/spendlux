b20776b7086a45ea0af76af9834123c3dc28d860
Rahul S Kumar <rahul.kumar@qburst.com>
2025-01-21 15:31:41 +0530

    Add Profile page, constants file and update page overflow of dashboard

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,11 @@
+"use Client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import React from "react";
 
 export default function Profile() {
   return (
-    <div className="justify-self-center">
+    <section about="profile" className="justify-self-center">
       <div className="rounded p-4 m-3">
         <Avatar className="w-44 h-44">
           <AvatarImage src="https://github.com/shadcn.png" />
@@ -19,6 +21,6 @@ export default function Profile() {
           <button className="font-semibold text-xs">logout</button>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
