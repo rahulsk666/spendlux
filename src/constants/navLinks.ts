06ff9caf493e6888f5df5d150a17e54ad87a4805
Rahul S Kumar <rahul.kumar@qburst.com>
2025-01-21 15:31:41 +0530

    Add Profile page, constants file and update page overflow of dashboard

--- src/constants/navLinks.ts
+++ src/constants/navLinks.ts
@@ -1,34 +1,11 @@
 export const linkItems = [
-  {
-    label: "Dashboard",
-    id: "home",
-    icon: "/home.svg",
-    href: "/",
-    title: "Dashboard",
-    subtitle: "your",
-  },
-  {
-    label: "Add New",
-    id: "create",
-    icon: "/plus.svg",
-    href: "/create",
-    title: "Transaction",
-    subtitle: "add",
-  },
+  { label: "Dashboard", id: "home", icon: "/home.svg", href: "/" },
+  { label: "Add New", id: "create", icon: "/plus.svg", href: "/create" },
   {
     label: "Analytics",
     id: "analytics",
     icon: "/bar_chart.svg",
     href: "/analytics",
-    title: "Analytics",
-    subtitle: "your",
   },
-  {
-    label: "All Transactions",
-    id: "transactions",
-    icon: "/list.svg",
-    href: "/transactions",
-    title: "Transactions",
-    subtitle: "all",
-  },
+  { label: "All Transactions", id: "list", icon: "/list.svg", href: "/list" },
 ];
