14de73fc83d21d169dc669f4a5cd7faa2fd27d2d
Rahul S Kumar <rahul.kumar@qburst.com>
2025-01-21 17:40:09 +0530

    Add page title and subtile to nav links

--- src/constants/navLinks.ts
+++ src/constants/navLinks.ts
@@ -1,11 +1,34 @@
 export const linkItems = [
-  { label: "Dashboard", id: "home", icon: "/home.svg", href: "/" },
-  { label: "Add New", id: "create", icon: "/plus.svg", href: "/create" },
+  {
+    label: "Dashboard",
+    id: "home",
+    icon: "/home.svg",
+    href: "/",
+    title: "Dashboard",
+    subtitle: "your",
+  },
+  {
+    label: "Add New",
+    id: "create",
+    icon: "/plus.svg",
+    href: "/create",
+    title: "Transaction",
+    subtitle: "add",
+  },
   {
     label: "Analytics",
     id: "analytics",
     icon: "/bar_chart.svg",
     href: "/analytics",
+    title: "Analytics",
+    subtitle: "your",
   },
-  { label: "All Transactions", id: "list", icon: "/list.svg", href: "/list" },
+  {
+    label: "All Transactions",
+    id: "list",
+    icon: "/list.svg",
+    href: "/list",
+    title: "Transactions",
+    subtitle: "all",
+  },
 ];
