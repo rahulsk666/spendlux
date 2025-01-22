45a4d2fc04429a98927ca9ae4405f246f9ff7988
Rahul S Kumar <rahul.kumar@qburst.com>
2025-01-22 11:38:15 +0530

    Add Create page, page transitions

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Profile() {
   return (
-    <section about="profile" className="justify-self-center">
+    <div className="justify-self-center">
       <div className="rounded p-4 m-3">
         <Avatar className="w-44 h-44">
           <AvatarImage src="https://github.com/shadcn.png" />
@@ -19,6 +19,6 @@ export default function Profile() {
           <button className="font-semibold text-xs">logout</button>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
