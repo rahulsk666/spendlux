44c18fe6e9d9bcfe33418449ecff9ba92fe30655
Rahul S Kumar <rahul.kumar@qburst.com>
2025-02-25 12:08:30 +0530

    add form for create page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,34 @@ import TransactionCard from "@/components/transactionCard";
 import Link from "next/link";
 
 export default function Home() {
+  const transactions = [
+    { success: false },
+    { success: true },
+    { success: false },
+    { success: true },
+    { success: false },
+    { success: false },
+    { success: true },
+    { success: false },
+  ];
+
   return (
-    <section>
-      <div className="mt-2">
-        <div className="rounded">
-          <SemiCircleProgressBar />
+    <section className="mt-2">
+      <div className="rounded">
+        <SemiCircleProgressBar />
+      </div>
+
+      <div className="grid grid-cols-2 m-2 p-3 text-xs font-normal">
+        <div>Recent Transactions</div>
+        <div className="text-right underline decoration-white/25">
+          <Link href="/">View All</Link>
         </div>
-        <div className="grid grid-cols-2 m-2 p-3 text-xs font-normal">
-          <div className="col-span-1">Recent Transactions</div>
-          <div className="text-right underline decoration-white/25">
-            <Link href={"/"}>View All</Link>
-          </div>
-        </div>
-        <div>
-          <TransactionCard success={false} />
-          <TransactionCard success={true} />
-          <TransactionCard success={false} />
-          <TransactionCard success={true} />
-          <TransactionCard success={false} />
-          <TransactionCard success={false} />
-          <TransactionCard success={true} />
-          <TransactionCard success={false} />
-        </div>
+      </div>
+
+      <div>
+        {transactions.map((transaction, index) => (
+          <TransactionCard key={index} success={transaction.success} />
+        ))}
       </div>
     </section>
   );
