797a93099479fd1e739c0f6ac45a6d100a334730
Rahul S Kumar <rahul.kumar@qburst.com>
2025-02-25 12:08:30 +0530

    add form for create page.

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -8,46 +8,40 @@ import { usePathname } from "next/navigation";
 import { linkItems } from "@/constants/navLinks";
 
 export default function AppBar() {
-  const [activeNav, setActiveNav] = useState("profile");
-  const handleNav = (currentNav: string) => {
-    setActiveNav(currentNav);
-  };
+  const [activeNav, setActiveNav] = useState("");
   const pathName = usePathname();
+
   useEffect(() => {
-    linkItems.map((item) => {
-      if (item.href == pathName) {
-        setActiveNav(item.id);
-      }
-    });
+    const currentItem = linkItems.find((item) => item.href === pathName);
+    if (currentItem) setActiveNav(currentItem.id);
   }, [pathName]);
+
   return (
     <div className="fixed bottom-2 bg-appbar-primary inset-x-0 flex flex-row rounded-full justify-self-center p-2">
-      {linkItems.map((item) => {
-        return (
-          <Link
-            key={item.id}
-            href={item.href}
-            className="p-0 rounded-full min-w-fit"
+      {linkItems.map((item) => (
+        <Link
+          key={item.id}
+          href={item.href}
+          className="p-0 rounded-full min-w-fit"
+        >
+          <IconButton
+            iconName={item.id}
+            text={item.label}
+            className="p-0"
+            spanClass="text-xs font-normal"
+            handleNav={setActiveNav}
+            activeNav={activeNav}
           >
-            <IconButton
-              iconName={item.id}
-              text={item.label}
-              className="p-0"
-              spanClass="text-xs font-normal"
-              handleNav={handleNav}
-              activeNav={activeNav}
-            >
-              <Image
-                src={item.icon}
-                alt={item.id}
-                width={25}
-                height={25}
-                className="p-1"
-              />
-            </IconButton>
-          </Link>
-        );
-      })}
+            <Image
+              src={item.icon}
+              alt={item.id}
+              width={25}
+              height={25}
+              className="p-1"
+            />
+          </IconButton>
+        </Link>
+      ))}
     </div>
   );
 }
