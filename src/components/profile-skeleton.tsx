1a9381fc22b6369eb84c59e014f597ba038d4ff0
Rahul S Kumar <rahul.kumar@qburst.com>
2025-04-10 14:49:14 +0530

    add header skeleton

--- src/components/profile-skeleton.tsx
+++ src/components/profile-skeleton.tsx
@@ -1,27 +1,25 @@
-import { Skeleton } from "./ui/skeleton"
-
 export function ProfileSkeleton() {
   return (
     <div className="justify-self-center">
       <div className="rounded p-4 m-3">
         {/* Avatar */}
         <div className="flex justify-center">
-          <Skeleton className="w-44 h-44 rounded-full" />
+          <div className="w-44 h-44 rounded-full bg-muted animate-pulse" />
         </div>
 
         {/* Name */}
-        <div className="pt-4 text-center">
-          <Skeleton className="h-6 w-[200px] mx-auto" />
+        <div className="pt-4 text-center font-bold text-lg">
+          <div className="h-6 w-[200px] mx-auto bg-muted animate-pulse rounded" />
         </div>
 
         {/* Email */}
-        <div className="pt-2 text-center">
-          <Skeleton className="h-4 w-[150px] mx-auto" />
+        <div className="pt-2 text-center text-[10px]">
+          <div className="h-4 w-[150px] mx-auto bg-muted animate-pulse rounded" />
         </div>
 
         {/* Logout Button */}
-        <div className="mt-6 p-3 px-5 text-center">
-          <Skeleton className="h-10 w-[100px] mx-auto rounded-full" />
+        <div className="mt-6 p-3 px-5 text-center bg-appbar-blue rounded-full">
+          <div className="h-6 w-[60px] mx-auto bg-white/20 animate-pulse rounded-full" />
         </div>
       </div>
     </div>
