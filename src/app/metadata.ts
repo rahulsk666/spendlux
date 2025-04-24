bb53f688c78905f931dc6c6ba3c8573cea0dab90
Rahul S Kumar <rahul.kumar@qburst.com>
2025-04-24 18:12:55 +0530

    update metadat for removing pinch zoom

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -3,4 +3,5 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Spendlux",
   description: "Finance manager",
-}; 
+  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+};
