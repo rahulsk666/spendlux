45ec88a19770972b810fec889853f5a074a338f8
Rahul S Kumar <rahul.kumar@qburst.com>
2025-05-29 17:45:08 +0530

    feat: implement dashboard and profile loading components, refactor authentication handling with Zustand

--- src/app/(auth)/profile/page.tsx
+++ src/app/(auth)/profile/page.tsx
@@ -1,32 +1,40 @@
 import { fetchProfile } from "@/lib/profileActions";
-import { signOut } from "./action";
+import { signOut } from "@/app/(auth)/profile/action";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Suspense } from "react";
+import Loading from "@/app/(auth)/profile/loading";
 
 export default async function Profile() {
   const user = await fetchProfile();
-  
+
   return (
-    <form className="justify-self-center">
-      <div className="rounded p-4 m-3">
-        <Avatar className="w-44 h-44">
-          <AvatarImage src={user?.user_metadata?.avatar_url} loading="eager" />
-          <AvatarFallback>
-            {user?.user_metadata?.full_name?.charAt(0) ||
-              user?.email?.charAt(0)}
-          </AvatarFallback>
-        </Avatar>
-        <div className="pt-4 text-center font-bold text-lg">
-          <span>{user?.user_metadata.full_name || "User"}</span>
+    <Suspense fallback={<Loading />}>
+      <form className="justify-self-center">
+        <div className="rounded p-4 m-3">
+          <Avatar className="w-44 h-44">
+            <AvatarImage
+              alt="U"
+              src={user?.user_metadata?.avatar_url}
+              loading="eager"
+            />
+            <AvatarFallback>
+              {user?.user_metadata?.full_name?.charAt(0) ||
+                user?.email?.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
+          <div className="pt-4 text-center font-bold text-lg">
+            <span>{user?.user_metadata.full_name || "User"}</span>
+          </div>
+          <div className="pt-2 text-center text-[10px]">
+            <span>{user?.email}</span>
+          </div>
+          <div className="mt-6 p-3 px-5 text-center bg-appbar-blue rounded-full">
+            <button formAction={signOut} className="font-semibold text-xs">
+              Logout
+            </button>
+          </div>
         </div>
-        <div className="pt-2 text-center text-[10px]">
-          <span>{user?.email}</span>
-        </div>
-        <div className="mt-6 p-3 px-5 text-center bg-appbar-blue rounded-full">
-          <button formAction={signOut} className="font-semibold text-xs">
-            Logout
-          </button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </Suspense>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,63 +1,13 @@
-"use client";
+import { fetchProfile } from "@/lib/profileActions";
+import HeaderPage from "./header-page";
 
-import React, { useEffect, useState } from "react";
-// import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { usePathname } from "next/navigation";
-import { linkItems } from "@/constants/navLinks";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { useUserStore } from "@/store/userStore";
-// import { HeaderSkeleton } from "./header-skeleton";
-
-export default function Header() {
-  // const [user, setUser] = useState<User | null>(null);
-  const user = useUserStore((state) => state.user);
-  const pathName = usePathname();
-  const [pageName, setPageName] = useState({
-    title: "",
-    subtitle: "",
-  });
-
-  useEffect(() => {
-    linkItems.map((items) => {
-      if (items.href == pathName) {
-        setPageName({
-          title: items.title,
-          subtitle: items.subtitle,
-        });
-      } else if ("/profile" == pathName) {
-        setPageName({
-          title: "Profile",
-          subtitle: "your",
-        });
-      }
-    });
-  }, [pathName]);
+export default async function Header() {
+  const user = await fetchProfile();
 
   return (
     <div>
       <div className="grid grid-cols-3 text-white">
-        <div className="col-span-2 text-left text-xs p-5">
-          <p className="font-montserrat tracking-wider">
-            Welcome!
-            <span className="font-bold text-base pl-1">
-              {user?.user_metadata?.name || "User"}
-            </span>
-          </p>
-          <p>
-            {pageName.subtitle}
-            <span className="font-bold text-lg pl-2">{pageName.title}</span>
-          </p>
-        </div>
-        <a href="/profile" className="flex justify-end p-4">
-          {/* <div className="w-12 h-12 rounded-full bg-gray-700 animate-pulse"></div> */}
-          <Avatar className="w-12 h-12">
-            <AvatarImage
-              src={user?.user_metadata?.avatar_url || undefined}
-              loading="eager"
-            />
-            <AvatarFallback>U</AvatarFallback>
-          </Avatar>
-        </a>
+        <HeaderPage user={user} />
       </div>
     </div>
   );
